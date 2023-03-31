1f8a5a8cc8dc1fa7612a7b43771bb1866f31193f
yichien <kevinyichienlee@gmail.com>
2023-03-31 23:49:13 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hi
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
