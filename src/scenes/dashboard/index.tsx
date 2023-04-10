d8b594b923739c9ae7caeb6c6ce88687b8e2aeed
yichien <kevinyichienlee@gmail.com>
2023-04-10 21:56:10 +0800

    seperate rows

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
-import DashboardBox from "@/components/DashboardBox";
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-
-type Props = {};
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -49,9 +49,8 @@ const gridTemplateSmallScreens = `
 "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const { palette } = useTheme();
 
   return (
     <Box
@@ -73,16 +72,9 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
